c0704317936047c3e9475091ac3280eb35e25156
hcc <3291615056@qq.com>
2022-04-25 16:28:25 +0800

    1

--- src/main/java/com/hz/qny/QnyApplication.java
+++ src/main/java/com/hz/qny/QnyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QnyApplication {
-    int i=0;
+
 
 
     public static void main(String[] args) {
