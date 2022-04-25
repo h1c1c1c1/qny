78482a867ff98ec3536ccf1bb1b5a1f26b5cb1dd
hcc <3291615056@qq.com>
2022-04-25 16:02:04 +0800

    Initial commit

---
+++ src/main/java/com/hz/qny/QnyApplication.java
@@ -0,0 +1,18 @@
+package com.hz.qny;
+
+import com.hz.qny.text.ssq;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnyApplication {
+    private void show(){
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run( QnyApplication.class, args );
+
+    }
+
+}
---
+++ src/main/java/com/hz/qny/tool/VariableName.java
@@ -0,0 +1,16 @@
+package com.hz.qny.tool;
+
+public class VariableName {
+    //七牛云AK
+    public static final String accessKey="zuAjf8Gd_mwc3fu-vff7HmqcKLIIjaMIjAb4xXH6";
+    //七牛云SK
+    public static final String secretKey="ossyc9hmB4R8-U46shyYHUENxfPlnTA0RqzMJVrB";
+
+    //    七牛存储空间名
+    public static final String bucket = "hccc";
+    //    七牛默认域名
+    public static final String domain = "http://ras2oxb4i.hn-bkt.clouddn.com";
+
+}
+
+
